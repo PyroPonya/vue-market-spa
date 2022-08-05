ea96fc7c604b9106c9b7c18c548647a74d48d35f
PyroPonya <43729142+PyroPonya@users.noreply.github.com>
2022-08-05 22:29:41 +0300

    fixed building macro eror

---
+++ components/indexComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container_background">
+    <div class="container_flex">
+      <itemform-component />
+      <itemlist-component />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container_background {
+  background: rgba(255, 254, 251, 0.8);
+  min-height: 100vh;
+}
+.container_flex {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  padding: 32px;
+  max-width: 1376px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,5 @@
 <template>
-  <div class="container_background">
-    <div class="container_flex">
-      <itemform-component />
-      <itemlist-component />
-    </div>
+  <div>
+    <indexComponent />
   </div>
 </template>
-
-<style scoped>
-.container_background {
-  background: rgba(255, 254, 251, 0.8);
-  min-height: 100vh;
-}
-.container_flex {
-  margin: 0 auto;
-  display: flex;
-  flex-direction: row;
-  gap: 16px;
-  padding: 32px;
-  max-width: 1376px;
-}
-</style>
